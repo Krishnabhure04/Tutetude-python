ef899354ad7f96451ca34e6c76d05003e8687a09
Krishna Bhure <krishnabhure2004@gmail.com>
2025-05-11 19:48:53 +0530

    Add files via upload

---
+++ Task 1.py
@@ -0,0 +1,11 @@
+#arithmetic operator a=5 & b=2
+a=int(input('enter a value: '))
+b=int(input('enter a value: '))
+c=a+b
+print(c)
+d=a-b
+print(d)
+e=a*b
+print(e)
+f=a/b
+print(f)
---
+++ Task 2.py
@@ -0,0 +1,3 @@
+a=input('enter a first name: ')
+b=input('enter a last name: ')
+print("hello"+", " + a + " " + b+"!"+" Welcome"+" to"+" the"+" python"+" program")
